b314365371f6f95ed2fb5ad31969d543fc7593bd
Ryan Migaud <rmigaud@asu.edu>
2023-02-01 09:49:45 -0800

    spring component annotation

---
+++ src/main/java/annotations/component/ComponentApplication.java
@@ -0,0 +1,13 @@
+package annotations.component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentApplication.class, args);
+	}
+
+}
